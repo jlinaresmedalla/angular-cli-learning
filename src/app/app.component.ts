22af0502ce02cfd19b602dd6daa4ff0368a27a37
jlinaresmedalla <jlinaresmedalla@gmail.com>
2023-07-26 15:33:26 -0500

    Learning components, ngIF, ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message = "I'm read only!";
-  canEdit = false;
 
-  onEditClick() {
-    this.canEdit = !this.canEdit;
-    if (this.canEdit) {
-      this.message = 'You can edit me!';
-    } else {
-      this.message = "I'm read only!";
-    }
-  }
 }
